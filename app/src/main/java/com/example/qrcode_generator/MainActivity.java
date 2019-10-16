ac6fd7ad14a9955d70c1168e8b193a30afffcfca
Feng, Cheng-Chi <qwe987299@gmail.com>
2019-10-16 22:15:45 +0800

    QR Code 生成功能完成

--- app/src/main/java/com/example/qrcode_generator/MainActivity.java
+++ app/src/main/java/com/example/qrcode_generator/MainActivity.java
@@ -2,7 +2,16 @@ package com.example.qrcode_generator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button_ok = (Button) findViewById(R.id.button_ok);
+        button_ok.setOnClickListener(btn_ok);
     }
+
+    private Button.OnClickListener btn_ok = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            genCode(v);
+
+        }
+    };
+
+    public void genCode(View view) {
+        EditText input = (EditText) findViewById(R.id.input);
+        ImageView image_view = (ImageView) findViewById(R.id.image_view);
+        BarcodeEncoder encoder = new BarcodeEncoder();
+        try {
+            Bitmap bit = encoder.encodeBitmap(input.getText().toString(), BarcodeFormat.QR_CODE,
+                    250, 250);
+            image_view.setImageBitmap(bit);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
